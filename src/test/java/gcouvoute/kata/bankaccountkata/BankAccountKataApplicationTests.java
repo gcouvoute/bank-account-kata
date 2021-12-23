41f0fad5e049ff2d774a50d610af1282594ebf93
gcouvoute <guillaume.couvoute@gmail.com>
2021-12-23 16:12:33 +0100

    Initial commit

---
+++ src/test/java/gcouvoute/kata/bankaccountkata/BankAccountKataApplicationTests.java
@@ -0,0 +1,15 @@
+package gcouvoute.kata.bankaccountkata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountKataApplicationTests {
+
+    @Test
+    @SuppressWarnings("java:S2699")
+        // Context load doesn't need assertion
+    void contextLoads() {
+    }
+
+}
